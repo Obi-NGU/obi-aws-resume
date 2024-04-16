f0c0e163feac7795b9504bed6fed3da9aee69fb5
Obi-NGU <obinnamereni@gmail.com>
2024-04-16 12:52:26 +0100

    Initial Commit

---
+++ resume-code/scripts.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const timelineData = [
+        {
+            date: 'Jan 2024 - April 2024',
+            title: 'Junior Cloud Engineer',
+            description: 'The Generation Cloud Engineer Bootcamp for AWS is a comprehensive training program focused on AWS fundamentals, cloud architecture, infrastructure automation, security, DevOps practices, monitoring, scalability, and high availability. Participants gain hands-on experience through labs and case studies, preparing them for roles in designing, implementing, and managing cloud solutions on AWS.'
+        },
+        {
+            date: 'Jul 2020 - Sep 2020',
+            title: 'Data Science Intern',
+            description: 'The Data Science Internship offers hands-on experience in data analysis, machine learning, and statistical modeling. Interns learn to extract insights from data, develop predictive models, and communicate findings effectively. This internship provides exposure to real-world data projects and tools, preparing interns for entry-level roles in data science and analytics.'
+        },
+        {
+            date: 'Jul 2018 - Aug 2018',
+            title: 'Summer Marketing Intern',
+            description: 'The Summer Marketing Internship provides practical experience in various aspects of marketing, including market research, content creation, social media management, and campaign analytics. Interns gain exposure to marketing strategies, consumer behavior analysis, and digital marketing techniques. This internship equips interns with valuable skills and knowledge to contribute to marketing campaigns and initiatives, setting them on a path towards a career in marketing.'
+        }
+    ];
+
+    const timeline = document.getElementById('timeline');
+
+    timelineData.forEach((item, index) => {
+        let entry = document.createElement('div');
+        entry.classList.add('timeline-entry');
+        entry.setAttribute('id', 'entry-' + index);
+
+        let dot = document.createElement('div');
+        dot.classList.add('timeline-dot');
+
+        let date = document.createElement('div');
+        date.textContent = item.date;
+        date.classList.add('timeline-date');
+
+        let content = document.createElement('div');
+        content.classList.add('timeline-content');
+        content.setAttribute('id', 'content-' + index);
+
+        let title = document.createElement('h3');
+        title.textContent = item.title;
+
+        let description = document.createElement('p');
+        description.textContent = item.description;
+        description.style.width = '50%'; // Set the width of the description to half the page
+
+        // Click event to toggle the description visibility
+        entry.addEventListener('click', function() {
+            description.style.display = description.style.display === 'none' ? 'block' : 'none';
+        });
+
+        content.appendChild(title);
+        content.appendChild(description);
+
+        entry.appendChild(dot);
+        entry.appendChild(date);
+        entry.appendChild(content);
+
+        timeline.appendChild(entry);
+    });
+
+   
+    // Visitor Counter Script
+    // Function to make a GET request to your API endpoint
+    function fetchVisitorCount() {
+        fetch('https://7ivhn4hkstgdcbabio2uj2ckcq0ieebg.lambda-url.eu-west-2.on.aws/')
+        .then(response => response.json())
+        .then(data => {
+            // Update the visitor count on the webpage
+            document.getElementById('visitorCount').textContent = data.visitorCount;
+        })
+        .catch(error => console.error('Error fetching visitor count:', error));
+    }
+
+    // Call the function to fetch visitor count when the page loads
+    fetchVisitorCount();
+
+});
